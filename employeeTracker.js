8fa8db7cf51f4f35b9346833797bed32b9dcd4a1
Sharon Kim <skimilk19@gmail.com>
2021-03-15 15:55:56 -0400

    updated js file

--- employeeTracker.js
+++ employeeTracker.js
@@ -28,139 +28,184 @@ connection.connect((err) => {
 
 function mainMenu() {
     inquirer
-    .prompt({
-        name:"action",
-        tyoe:"list",
-        message:"Main Menu",
-        choices: [
-            "View all employees",
-            "View all employees by role",
-            "View all employees by department",
-            "View all employees by manager",
-            "Add employee",
-            "Add role",
-            "Add department",
-            "Update employee role",
-            "Update employee manager",
-            "Add employee",
-            "Delete employee",
-            "Delete role",
-            "Delete department",
-            "View department budgets"
-        ]           
-    })
-    .then((answer) => {
+        .prompt({
+            name: "action",
+            tyoe: "list",
+            message: "Main Menu",
+            choices: [
+                "View all employees",
+                "View all employees by role",
+                "View all employees by department",
+                "View all employees by manager",
+                "Add employee",
+                "Add role",
+                "Add department",
+                "Update employee role",
+                "Update employee manager",
+                "Add employee",
+                "Delete employee",
+                "Delete role",
+                "Delete department",
+                "View department budgets"
+            ]
+        })
+        .then((answer) => {
 
-        // Switch case depending on user option
-        switch (answer.action) {
-            case "View all employees":
-                viewAllEmp();
-                break;
+            // Switch case depending on user option
+            switch (answer.action) {
+                case "View all employees":
+                    viewAllEmp();
+                    break;
 
-            case "View all employees by department":
-                viewAllEmpByDept();
-                break;
+                case "View all employees by department":
+                    viewAllEmpByDept();
+                    break;
 
-            case "View all employees by role":
-                viewAllEmpByRole();
-                break;
+                case "View all employees by role":
+                    viewAllEmpByRole();
+                    break;
 
-            case "Add employee":
-                addEmp();
-                break;
+                case "Add employee":
+                    addEmp();
+                    break;
 
-            case "Add department":
-                addDept();
-                break;
-            case "Add role":
-                addRole();
-                break;
-            case "Update employee role":
-                updateEmpRole();
-                break;
-            case "Update employee manager":
-                updateEmpMngr();
-                break;
-            case "View all employees by manager":
-                viewAllEmpByMngr();
-                break;
-            case "Delete employee":
-                deleteEmp();
-                break;
-            case "View department budgets":
-                viewDeptBudget();
-                break;
-            case "Delete role":
-                deleteRole();
-                break;
-            case "Delete department":
-                deleteDept();
-                break;
-        }
-    });
+                case "Add department":
+                    addDept();
+                    break;
+                case "Add role":
+                    addRole();
+                    break;
+                case "Update employee role":
+                    updateEmpRole();
+                    break;
+                case "Update employee manager":
+                    updateEmpMngr();
+                    break;
+                case "View all employees by manager":
+                    viewAllEmpByMngr();
+                    break;
+                case "Delete employee":
+                    deleteEmp();
+                    break;
+                case "View department budgets":
+                    viewDeptBudget();
+                    break;
+                case "Delete role":
+                    deleteRole();
+                    break;
+                case "Delete department":
+                    deleteDept();
+                    break
+            }
+        });
 }
 
-// View all employees
-
+// View all employees 
 function viewAllEmp() {
-    // this is teh query to view all employees
 
-    let query = "Select e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary, concat(m.first_name, ' ' ,  m.last_name) AS manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id ORDER BY ID ASC";
+    // Query to view all employees
+    let query = "SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary, concat(m.first_name, ' ' ,  m.last_name) AS manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id ORDER BY ID ASC";
 
     // Query from connection
-    connection.query(query, function(err, res){
-        if(err) return err;
+    connection.query(query, function (err, res) {
+        if (err) return err;
         console.log("\n");
 
-        // display the results 
+        // Display query results using console.table
         console.table(res);
 
-        //back to main menu
+        //Back to main menu
         mainMenu();
     });
 }
 
 // View all employees by department
+function viewAllEmpByDept() {
 
-function viewAllEmpByDept () {
-    // Set global array to store departmnet names
+    // Set global array to store department names
     let deptArr = [];
 
     // Create new connection using promise-sql
-    promisemysql.createConnection(connectionProperties).then((conn) => {
-        // query names of dept
+    promisemysql.createConnection(connectionProperties
+    ).then((conn) => {
+
+        // Query just names of departments
         return conn.query('SELECT name FROM department');
-        .then(function(value){
-            //Place all names within deptArr
-            deptQuery = value;
-            for (i=0; i < value.length; i++){
-                deptArr.push(value[i].name);
-            }
-            .then(() => {
-                //Prompt user to select department 
-                inquirer.prompt({
-                    name: "department",
-                    type: "list",
-                    message: "Which department would you like to search for?",
-                    choices: deptArr
-                })
-                .then((answer) => {
-                    //Query all employees depending on selected department
-                    const query = `SELECT e.id AS ID, e.first_name AS 'First Name', e.last_name AS 'Last Name', role.title AS Title, department.name AS Department, role.salary AS Salary, concat(m.first_name, ' ' ,  m.last_name) AS Manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id WHERE department.name = '${answer.department}' ORDER BY ID ASC`;
-                    connection.query(query, (err, res) => {
-                        if(err) return err;
+    }).then(function (value) {
 
-                        // Show result in console.table
+        // Place all names within deptArr
+        deptQuery = value;
+        for (i = 0; i < value.length; i++) {
+            deptArr.push(value[i].name);
 
-                        console.log("\n");
-                        console.table(res);
+        }
+    }).then(() => {
 
-                        //Back to main menu
-                        mainMenu();
-                    });
+        // Prompt user to select department from array of departments
+        inquirer.prompt({
+            name: "department",
+            type: "list",
+            message: "Which department would you like to search?",
+            choices: deptArr
+        })
+            .then((answer) => {
+
+                // Query all employees depending on selected department
+                const query = `SELECT e.id AS ID, e.first_name AS 'First Name', e.last_name AS 'Last Name', role.title AS Title, department.name AS Department, role.salary AS Salary, concat(m.first_name, ' ' ,  m.last_name) AS Manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id WHERE department.name = '${answer.department}' ORDER BY ID ASC`;
+                connection.query(query, (err, res) => {
+                    if (err) return err;
+
+                    // Show results in console.table
+                    console.log("\n");
+                    console.table(res);
+
+                    // Back to main menu
+                    mainMenu();
                 });
             });
-            
-        })
-    })
+    });
+}
+
+// view all employees by role
+function viewAllEmpByRole() {
+
+    // set global array to store all roles
+    let roleArr = [];
+
+    // Create connection using promise-sql
+    promisemysql.createConnection(connectionProperties)
+        .then((conn) => {
+
+            // Query all roles
+            return conn.query('SELECT title FROM role');
+        }).then(function (roles) {
+
+            // Place all roles within the roleArry
+            for (i = 0; i < roles.length; i++) {
+                roleArr.push(roles[i].title);
+            }
+        }).then(() => {
+
+                // Prompt user to select a role 
+
+        inquirer.prompt({
+            name: "role",
+            type: "list",
+            message: "Which role would you like to search?",
+            choices: roleArr
+        })    
+        .then((answer) => {
+
+            // Query all employees by role selected by user
+            const query = `SELECT e.id AS ID, e.first_name AS 'First Name', e.last_name AS 'Last Name', role.title AS Title, department.name AS Department, role.salary AS Salary, concat(m.first_name, ' ' ,  m.last_name) AS Manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id WHERE role.title = '${answer.role}' ORDER BY ID ASC`;
+            connection.query(query, (err, res) => {
+                if(err) return err;
+
+                // show results using console.table
+                console.log("\n");
+                console.table(res);
+                mainMenu();
+            });
+        });
+    });
 }
