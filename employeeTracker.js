95a39b2065f66dc90620693e1d447a05e35c3ff1
Sharon Kim <skimilk19@gmail.com>
2021-03-14 22:33:32 -0400

    employeeTracker.js init

--- employeeTracker.js
+++ employeeTracker.js
@@ -25,3 +25,78 @@ connection.connect((err) => {
     console.log("\n WELCOME TO THE EMPLOYEE TRACKER \n");
     mainMenu();
 });
+
+function mainMenu() {
+    inquirer
+    .prompt({
+        name:"action",
+        tyoe:"list",
+        message:"Main Menu",
+        choices: [
+            "View all employees",
+            "View all employees by role",
+            "View all employees by department",
+            "View all employees by manager",
+            "Add employee",
+            "Add role",
+            "Add department",
+            "Update employee role",
+            "Update employee manager",
+            "Add employee",
+            "Delete employee",
+            "Delete role",
+            "Delete department",
+            "View department budgets"
+        ]           
+    })
+    .then((answer) => {
+
+        // Switch case depending on user option
+        switch (answer.action) {
+            case "View all employees":
+                viewAllEmp();
+                break;
+
+            case "View all employees by department":
+                viewAllEmpByDept();
+                break;
+
+            case "View all employees by role":
+                viewAllEmpByRole();
+                break;
+
+            case "Add employee":
+                addEmp();
+                break;
+
+            case "Add department":
+                addDept();
+                break;
+            case "Add role":
+                addRole();
+                break;
+            case "Update employee role":
+                updateEmpRole();
+                break;
+            case "Update employee manager":
+                updateEmpMngr();
+                break;
+            case "View all employees by manager":
+                viewAllEmpByMngr();
+                break;
+            case "Delete employee":
+                deleteEmp();
+                break;
+            case "View department budgets":
+                viewDeptBudget();
+                break;
+            case "Delete role":
+                deleteRole();
+                break;
+            case "Delete department":
+                deleteDept();
+                break;
+        }
+    });
+}
+
